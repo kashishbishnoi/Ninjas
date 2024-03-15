ca95369c91803f266e7e7197480633db1fe15d73
harshitalamba1 <156739620+harshitalamba1@users.noreply.github.com>
2024-03-15 23:45:18 +0530

    Hangman.py
    added more functionality of hints , figures ,and if the user wins the game is terminated and choice generated of asking to play again or not

--- Hangman.py
+++ Hangman.py
@@ -1,62 +1,116 @@
 # HANGMAN GAME
+print("\nWELCOME TO THE GAME !!!")
+print("THIS GAME GIVES THE USER 10 CHANCES TO GUESS A WORD.")
+print("THE USER WILL GET 1 HINT CHANCE IF CHOSEN.") 
+
 import random
-words=["cat","bat","dad","net","maid","kit","dog","sim"]
-choice="y"
-c=0
-i=0
-figure=['''
+
+words = {
+    "Australia": "HINT : THE COUNTRY WHICH IS THIRD MOST POPULAR IN WORLD....",
+    "Malawi": "HINT : The indigenous ethnic groups of it have a rich tradition of basketry",
+    "Brazil": "HINT : Soccer is the most popular sport of this country",
+    "Norway": "HINT : It is a Nordic country in Northern Europe, situated on the Scandinavian Peninsula.",
+    "Canada": "HINT : CN TOWER is the most famous in this country",
+    "Denmark": "HINT : It is almost entirely surrounded by water, which makes sense when you consider its Viking history.",
+    "Serbia": "HINT : It is home to roughly 7 million inhabitants and it lies at the crossroads of Southeast and central Europe. Belgrade, the capital of Serbia is ranked among the largest and oldest cities in southeastern",
+    "Monaco": "HINT : It is a small-city state on the French Riviera, known as a 'Billionaires' Playground. It is famous for its grand casinos, designer shopping centers, opulent bars and clubs, and a human-made beach",
+    "Qatar": "HINT : It was described as a famous horse and camel breeding centre during the Umayyad period.",
+    "India": "HINT : It is known for its diverse culture, history, natural landscapes, languages, classical dances, Bollywood, spirituality, natural beauty, and warm, welcoming citizens."
+}
+
+choice = "y"
+c = 0
+i = 0
+figure = [
+    '''
     
     +---+
        |
        |
        |
-      ===''','''
+      ===''',
+    '''
       
    +---+
    O   |
        |
        |
-      ===''', '''
+      ===''', 
+    '''
    +---+
    O   |
    |   |
        |
-      ===''','''
+      ===''',
+    '''
    +---+
    O   |
   /|   |
        |
-      ===''', '''
+      ===''', 
+    '''
    +---+
    O   |
   /|\  |
        |
-      ===''','''
+      ===''',
+    '''
    +---+
    O   |
   /|\  |
- /     |
-     ===''','''
+  /    |
+     ===''',
+    '''
    +---+
    O   |
   /|\  |
   / \  |
-       ==='''
+     ==='''
 ]
-w=random.choice(words) 
-while(choice=="y"):
-    for j in range(10):
-        c=c+1
 
+while choice == "y":
+    guess = 0
+    word = random.choice(list(words.keys()))
+    word_hint = words[word]
+    word_length = len(word)
+    guesses = 0
+    
+    print("\n\nWELCOME TO THE GAME !!!")
+    print("GUESS A WORD AND TRY TO FILL ")
+    for k in range(word_length):
+        print("_ ", end='')
+    print("\n")
+    
+    for j in range(10):
+        c += 1
+        letter = input("Enter any letter: ").lower()
         
-        letter=input("enter any letter").lower()
-        if letter in w:
-            print("Congratulations!")
-            print("you are close")
-            print("Attempts left:",10-c)
-        else :
-            print("Better luck next time")
-            print("Attempts left:",10-c)
+        if len(letter) != 1 or not letter.isalpha():
+            print("Please enter a single letter.")
+            continue
+        
+        if letter in word.lower():
+            print("You are one step closer!")
+            print("KEEP IT UP!")
+            print("Attempts left:", 10 - c)
+            guesses += word.lower().count(letter)
+            if guesses == word_length:
+                print("\nWELL DONE")
+                print("CONGRATULATIONS!")
+                print("YOU SUCCEEDED!")
+                break
+        else:
+            print("\nBetter luck next time")
+            print("Attempts left:", 10 - c)
+            print(figure[i])
+            i += 1
+            if i >= len(figure):
+                print("\nOH! YOU LOST")
+                print("****************Game Ended******************")
+                break
             
-            
-    choice=input("enter y if want to continue in game")
+            hint_choice = input("Want a hint? Enter 'y' for yes and 'n' for no: ")
+            if hint_choice.lower() == "y":
+                print(word_hint)
+                
+    choice = input("\nEnter 'y'  if you want to continue playing or 'n' for exit: ")
